faa70094861209a0708c31a3999c64abe589b7f4
Dave Nieuwenhuijzen <dave.nieuwenhuijzen@gmail.com>
2023-11-04 19:53:21 +0100

    Added required_providers

--- terraform/root.tf
+++ terraform/root.tf
@@ -6,5 +6,12 @@ module "acls" {
 }
 
 terraform {
+  required_providers {
+    nomad = {
+      source  = "hashicorp/nomad"
+      version = "~> 2.0.0"
+    }
+  }
+
   required_version = ">=1.6.3"
 }
