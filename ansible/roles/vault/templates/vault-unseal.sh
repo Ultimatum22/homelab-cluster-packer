9542d7679023d3728935f8b76cd1cf683c1d25e8
Dave Nieuwenhuijzen <dave.nieuwenhuijzen@gmail.com>
2023-12-28 09:45:47 +0100

    Fixed vault config unseal issues

--- ansible/roles/vault/templates/vault-unseal.sh
+++ ansible/roles/vault/templates/vault-unseal.sh
@@ -13,18 +13,18 @@ token=$(egrep 'Initial Root Token' $secretsfile | awk '{print $4}')
 source /etc/profile
 
 for k in $keys; do
-    vault operator unseal $k >/dev/null
+    export VAULT_ADDR=http://127.0.0.1:8200 ; vault operator unseal $k >/dev/null
 done
 
 sleep 2
 
-if [ "$(vault status | grep Sealed | awk '{print $2}')" == "true" ]; then
+if [ "$(export VAULT_ADDR=http://127.0.0.1:8200 ; vault status | grep Sealed | awk '{print $2}')" == "true" ]; then
     echo "ERROR: vault unsealing failed"
     exit 2
 fi
 
 # authenticate using the root token
-vault login $token >/dev/null
+export VAULT_ADDR=http://127.0.0.1:8200 ; vault login $token >/dev/null
 
 [[ $? -gt 0 ]] && echo "ERROR: vault auth failed" && exit 3
 
